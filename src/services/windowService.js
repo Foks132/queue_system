004ac844a94ad1f21c07aa5fe6605fa59e2d687e
Foks <saaamushkalov@gmail.com>
2025-05-10 22:06:41 +0500

    fix

--- src/services/windowService.js
+++ src/services/windowService.js
@@ -8,13 +8,13 @@ export const WindowService = {
             } 
         });
         if (!window) throw new Error("Window not found");
-        const userPermissions = await dbPrisma.userPermission.findMany({
-            where: { 
-                userId: Number(userId), 
-            },
-        });
-        const hasPermission = userPermissions.some(permission => permission.appealType === window.type);
-        if (!hasPermission) throw new Error("User does not have permission to join this window");
+        // const userPermissions = await dbPrisma.userPermission.findMany({
+        //     where: { 
+        //         userId: Number(userId), 
+        //     },
+        // });
+        // const hasPermission = userPermissions.some(permission => permission.appealType === window.type);
+        // if (!hasPermission) throw new Error("User does not have permission to join this window");
         const user = await dbPrisma.user.update({
             where: { 
                 id: Number(userId),
